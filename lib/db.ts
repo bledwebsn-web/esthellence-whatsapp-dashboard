dbf29c70e54d4c55f56f20e3d09816e6fbf17379
Moustafa <bledwebsn@gmail.com>
2026-05-26 15:46:24 +0000

    Connect webhook to local PostgreSQL

---
+++ lib/db.ts
@@ -0,0 +1,22 @@
+﻿import { Pool } from "pg";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var pgPool: Pool | undefined;
+}
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is missing");
+}
+
+export const db =
+  global.pgPool ??
+  new Pool({
+    connectionString,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.pgPool = db;
+}
